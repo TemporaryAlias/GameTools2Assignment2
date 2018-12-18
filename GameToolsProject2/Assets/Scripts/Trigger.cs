d69672bdf6b62d060e09a04d98984f3dbdeded07
TemporaryAlias <38929684+TemporaryAlias@users.noreply.github.com>
2018-12-18 18:10:30 +0000

    Finished level layout

---
+++ GameToolsProject2/Assets/Scripts/Trigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour {
+
+    [SerializeField] UnityEvent enterEvent;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            enterEvent.Invoke();
+        }
+    }
+
+}
